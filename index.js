193fac4c1ab95401174a11d18336d40ef440789d
Ahmed Sami <ahmadsami990990@gmail.com>
2025-01-20 02:52:07 +0200

    modified index.js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import express from "express";
 import bootstrap from "./src/app.controller.js";
-import https from 'https'
-import fs from 'fs'
-import dotenv from 'dotenv'
-dotenv.config()
+// import https from "https";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 // import crypto from 'crypto'
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 bootstrap(app, express);
 const options = {
-  key: fs.readFileSync("./cert.key"),  // private key
+  key: fs.readFileSync("./cert.key"), // private key
   cert: fs.readFileSync("./cert.crt"), // certificate
-}
-https.createServer(options, app).listen(port, (err) => {
+};
+// https.createServer(options, app).listen(port, (err) => {
+//   if (!err) {
+//     console.log("server connected successfully");
+//   } else {
+//     console.log(`error connecting server , ${err}`);
+//   }
+// });
+app.listen(port, (err) => {
   if (!err) {
     console.log("server connected successfully");
   } else {
